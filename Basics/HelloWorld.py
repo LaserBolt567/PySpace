2d79ef91ce1411bbcb14e5e1997fb7d092af9eb7
Divakar Babu Budumuru <divakar.budumuru@netenrich.com>
2019-03-02 23:07:53 +0530

    Hello world ml programme added

--- Basics/HelloWorld.py
+++ Basics/HelloWorld.py
@@ -1,2 +1,6 @@
 name = input("Enter your name: ")
 print("Hello "+name)
+
+# This is a module with module name HelloWorld. We can access this module name __name__
+
+print("This module name is " + __name__)
